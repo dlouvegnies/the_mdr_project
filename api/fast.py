d1607955545708b5c454d184dde12e71be85219d
Denis Louvegnies <dlouvegnies@users.noreply.github.com>
2024-03-19 10:56:12 +0100

    API squelette

---
+++ api/fast.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+#app.state.model =load_model()
+
+"""
+To launch the server :
+uvicorn api.fast:app --reload
+"""
+
+# Allowing all middleware is optional, but good practice for dev purposes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/get_one_news_to_learn")
+def get_one_news_to_learn(user_id:int):
+    """
+    Diplay a news to know if the user like it or not, in order to learn his tastes
+    """
+    #Your code here
+    pass
+
+
+
+@app.get("/save_one_learning")
+def save_one_learning(user_id:int, news_id:int, value:int):
+    """
+    Save, for the user, his taste for this news
+    value = 0 or 1
+    """
+    #Your code here
+    pass
+
+
+@app.get("/get_one_news_to_evaluate")
+def get_one_news_to_evaluate(user_id:int):
+    """
+    Diplay a news (a prediction) that the user is supposed to like.
+    """
+    pass
+
+
+@app.get("/save_one_evaluation")
+def save_one_evaluation(user_id:int, news_id:int, value:int):
+    """
+    Save, for the user, if the prediction to like this news is right or wrong
+    value = 0 or 1
+    """
+    #Your code here
+    pass
+
+
+
+
+@app.get("/")
+def root():
+    # YOUR CODE HERE
+    return {'greeting': 'Hello'}
