2a4aee5c4dbd0dababa37cca661a5c9f1e2b6471
Mathieu Arru <mathieu.arru.n@gmail.com>
2024-03-21 11:41:49 +0100

    user account and api

--- interface/news_page.py
+++ interface/news_page.py
@@ -6,56 +6,6 @@ import os
 from ml_logic.params import USER_ID, SERVICE_URL, MODE, LOCAL_URL
 
 base_url = SERVICE_URL if MODE == 'SERVICE' else LOCAL_URL
-def login_page():
-    st.title("Login")
-    # Formulaire de connexion
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    if st.button("Login"):
-        api_url = os.path.join(base_url, 'login')
-        params = {'username': username,
-                  'password': password}
-        response = requests.get(api_url, params=params)
-
-        if response.status_code == 200:
-            # AFFICHER LA PAGE TU ES LOGGE C'est bon
-            pass
-        else:
-            st.error(f"Failed to login from API. Status code: {response.status_code}")
-            return None
-
-# Fonction pour la page d'inscription
-def signup_page():
-    st.title("Sign Up")
-    # Formulaire d'inscription
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    if st.button("Sign Up"):
-        api_url = os.path.join(base_url, 'signup')
-        params = {'username': username,
-                  'password': password}
-        response = requests.get(api_url, params=params)
-
-        if response.status_code == 200:
-            # AFFICHER LA PAGE TU ES INSCRIT C'est bon
-            pass
-        else:
-            st.error(f"Failed to signup from API. Status code: {response.status_code}")
-            return None
-
-# Fonction principale pour gérer la navigation entre les pages
-def main():
-    st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", ("Login", "Sign Up"))
-
-    if selection == "Login":
-        login_page()
-    elif selection == "Sign Up":
-        signup_page()
-
-if __name__ == "__main__":
-    main()
-
 
 def fetch_news_to_learn(user_id):
     api_url = os.path.join(base_url, 'get_one_news_to_learn')
@@ -101,6 +51,7 @@ def save_learning_feedback(news, feedback, user_id):
     else:
         st.error(f"Failed save data from API. Status code: {response.status_code}")
     return response
+
 
 def save_recommendation_feedback(news, feedback, user_id):
     api_url = os.path.join(base_url, 'save_one_evaluation')
@@ -203,26 +154,3 @@ def display_recommendation():
         elif thumb_down:
             st.error("You weren't interested in this news.")
             save_recommendation_feedback(news, False, USER_ID)
-
-
-def main():
-    st.sidebar.markdown(f"""
-    # The MDR Project
-    """)
-
-    st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", ("Selection", "Recommendations"))
-
-    if selection == "Selection":
-        st.sidebar.write("You're viewing selected articles.")
-
-        display_learning()
-
-
-    elif selection == "Recommendations":
-        st.sidebar.write("You're viewing recommendations.")
-
-        display_recommendation()
-
-if __name__ == "__main__":
-    main()
