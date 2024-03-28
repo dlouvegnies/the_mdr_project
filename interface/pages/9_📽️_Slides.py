19e9defcebeff2610dcaee9c003bbe08a05a2293
Mathieu Arru <mathieu.arru.n@gmail.com>
2024-03-28 12:56:17 +0100

    diapo

---
+++ interface/pages/9_📽️_Slides.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title('📽️ Google Slides')
+components.iframe(src="https://docs.google.com/presentation/d/e/2PACX-1vSVFTNaLUYn9w4tSECUaNsvhHbOgDBRGgY5duLUV6lXbPeV0nA67YijfZXEr5tR8UOuBJYVhdMweVk1/embed?start=true&loop=false&delayms=60000", height=500)
